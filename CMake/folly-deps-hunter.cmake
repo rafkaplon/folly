b351b4256a3d7147ce1f5a0a6f53a04b2734389f
Patrick White <patrick.white@youi.tv>
2018-05-29 09:50:15 -0400

    glog is a required dependency for the slim folly

--- CMake/folly-deps-hunter.cmake
+++ CMake/folly-deps-hunter.cmake
@@ -4,15 +4,15 @@ find_package(double-conversion CONFIG REQUIRED)
 set(DOUBLE_CONVERSION_LIBRARY double-conversion::double-conversion)
 set(DOUBLE_CONVERSION_INCLUDE_DIR "")
 
+hunter_add_package(glog)
+find_package(glog CONFIG REQUIRED)
+
 if(NOT FOLLY_MINIMAL_CONFIGURATION)
   hunter_add_package(Boost COMPONENTS context chrono date_time filesystem program_options regex system thread)
   find_package(Boost CONFIG REQUIRED COMPONENTS context chrono date_time filesystem program_options regex system thread)
 
   hunter_add_package(gflags)
   find_package(gflags CONFIG REQUIRED)
-
-  hunter_add_package(glog)
-  find_package(glog CONFIG REQUIRED)
 
   hunter_add_package(Libevent)
   find_package(Libevent CONFIG REQUIRED)
